bdede37ac1fd4f45e54c324409b7f96e8534c278
delvauxo <delvauxo@outlook.com>
2018-01-11 12:26:18 +0100

    add exercice 01

--- exe01/server.js
+++ exe01/server.js
@@ -1,7 +1,87 @@
-console.log('test')
+// var a = 3
+// function test() {
+//     var a = 5
+// }
 
-var a = 2
+// let a = 4
+// {
+//     let a = 3
+// }
+
+// console.log(a)
+
+// (function(msg){
+//     console.log(msg)
+// })('a')
+
+// var obj = {
+//     name: 'gilles',
+//     job: 'student',
+//     toString(){
+//         // return 'name is '+this.name+' job is '+this.job
+//         // or
+//         return `name is ${this.name} and job is ${this.job}`
+//     }
+// }
+// console.log(obj.toString())
+
+// const arr=[a,b]=['gilles', 'bertrand']
+// console.log(arr)
+// console.log(a)
+// console.log(b)
+
+
+
+// const numbers = [1,2,3,4,5,6,7,8,9]
+
+// function dblNumbers(arr){
+
+//     const arrDbl = []
+
+//     for (let i = 0, length = arr.length; i < length; i++) {
+        
+//         arrDbl.push(arr[i]*2)
+        
+//     }
+
+//     return arrDbl
+
+// }
+
+// console.log(dblNumbers(numbers))
+
+// OR
+
+// const numbers = [1,2,3,4,5,6,7,8]
+
+// function dblNumbers(arr) {
+//     // return arr.map(function(item){
+//     //     return item*2
+//     // })
+//     // OR
+//     return arr.map(item=>item*2)
+// }
+
+// console.log(dblNumbers(numbers))
 
 
 
 
+// const numbers = [1,2,3,4,5,6,7,8]
+
+// function sommeNumbers(arr) {
+
+//     const somme = arr.reduce((prev, current)=>{
+        
+//         return prev+current
+
+//     })
+
+//     return somme
+// }
+
+// console.log(sommeNumbers(numbers))
+
+const numbers = [1,2,3,4,5,6,7,8]
+
+console.log(numbers.filter(item => item%2 === 0))
